5f6b1d836344d578fdff176b9906de1824263eda
ajay <ajay.mahajan@wipro.com>
2016-12-29 22:53:45 +0530

    Changing Pom and product class for distributed caching

--- ch3-product/src/main/java/com/mycompany/product/entity/Product.java
+++ ch3-product/src/main/java/com/mycompany/product/entity/Product.java
@@ -1,5 +1,7 @@
 package com.mycompany.product.entity ;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Product {
+public class Product implements Serializable {
 
 	@Id
     @GeneratedValue(strategy=GenerationType.AUTO)
