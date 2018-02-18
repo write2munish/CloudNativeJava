4e7dfa3dec57c2c24320dfc44babc12094cab633
Munish K Gupta <write2munish@gmail.com>
2018-02-18 20:09:14 +0530

    changed the chapter numbers

--- chapter-09/product/src/main/resources/import.sql
+++ chapter-09/product/src/main/resources/import.sql
@@ -3,4 +3,4 @@
 insert into product(id, name, cat_Id) values (1, 'Apples', 1)
 insert into product(id, name, cat_Id) values (2, 'Oranges', 1)
 insert into product(id, name, cat_Id) values (3, 'Bananas', 1)
-insert into product(id, name, cat_Id) values (4, 'Carrots', 2)
+insert into product(id, name, cat_Id) values (4, 'Carrot', 2)
